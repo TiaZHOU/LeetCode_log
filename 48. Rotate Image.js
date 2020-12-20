92e06bef16f38bb429872e81c49fef3d376d887e
TiaZHOU <tiazhou@student.unimelb.edu.au>
2020-12-21 00:23:23 +1100

    48

--- 48. Rotate Image.js
+++ 48. Rotate Image.js
@@ -2,6 +2,7 @@
  * link: https://leetcode-cn.com/problems/rotate-image/
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
+ * method 1 : rotate one by one
  */
 var rotate = function(matrix) {
     let size = matrix.length;
@@ -17,6 +18,37 @@ var rotate = function(matrix) {
             matrix[size-1-i-j][i] = matrix[size-1-i][size-1-i-j];
             matrix[size-1-i][size-1-i-j] = matrix[i+j][size-1-i];
             matrix[i+j][size-1-i] = temp;
+        }
+    }
+
+};
+
+/**
+ * link: https://leetcode-cn.com/problems/rotate-image/
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ * method 2 : turn upside down then diagonal mirror swap whole image
+ */
+var rotate = function(matrix) {
+    let temp;
+    //loops for mirro swap
+    for(let i = 0; i < matrix.length/2; i++)
+    {
+        for(let j =0 ; j< matrix.length; j++)
+        {
+            temp = matrix[i][j];
+            matrix[i][j] = matrix[matrix.length-1-i][j];
+            matrix[matrix.length-1-i][j] = temp;
+        }
+    }
+    //loops for Diagonal mirror swap
+    for(let i=0; i < matrix.length; i++)
+    {
+        for (let j =0; j<= i; j++)
+        {
+            temp = matrix[i][j];
+            matrix[i][j]= matrix[j][i];
+            matrix[j][i] = temp;
         }
     }
 
