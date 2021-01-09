5c093991a3f83cabee864873726ef88038d5b289
TiaZHOU <tiazhou@student.unimelb.edu.au>
2021-01-10 00:31:29 +1100

    344

---
+++ offers_lib/344. Reverse String.js
@@ -0,0 +1,14 @@
+/**
+ * link: https://leetcode-cn.com/problems/reverse-string/
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let temp = '';
+    for(let i=0; i< s.length/2 ; i++)
+    {
+        temp = s[i];
+        s[i] = s[s.length-1-i];
+        s[s.length-1-i] = temp;
+    }
+};
