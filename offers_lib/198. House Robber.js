5ed894321fdd66f7d01effd7eb72b9239a293a60
TiaZHOU <tiazhou@student.unimelb.edu.au>
2021-01-10 00:25:52 +1100

    198

---
+++ offers_lib/198. House Robber.js
@@ -0,0 +1,25 @@
+/**
+ * link: https://leetcode-cn.com/problems/house-robber/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if(nums.length ==0)
+    {
+        return 0;
+    }
+    if(nums.length ==1)
+    {
+        return nums[0];
+    }
+
+    let dp =[];
+    dp[0] =  nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for(let i=2; i < nums.length; i++)
+    {
+        dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+    }
+    return Math.max(dp[nums.length-2], dp[nums.length-1]);
+};
