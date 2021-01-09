2e6b0bf158ea031c3ef953a48e6c7ec3be15928a
TiaZHOU <tiazhou@student.unimelb.edu.au>
2021-01-10 00:08:11 +1100

    1688

---
+++ offers_lib/1688. Count of Matches in Tournament.js
@@ -0,0 +1,22 @@
+/**
+ * link :https://leetcode-cn.com/problems/count-of-matches-in-tournament/
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function(n) {
+    let ans =0;
+    while(n > 1)
+    {
+        if(n% 2 ==0)
+        {
+            n = n / 2;
+            ans = ans + n;
+        }
+        else
+        {
+            n = (n+1)/2 ;
+            ans = ans + n -1;
+        }
+    }
+    return ans;
+};
