d3d05f90d6696be9e2788c23b76080721c72be29
TiaZHOU <tiazhou@student.unimelb.edu.au>
2021-01-13 00:31:49 +1100

    offer62

--- offers_lib/62.last number in the round.js
+++ offers_lib/62.last number in the round.js
@@ -5,5 +5,27 @@
  * @return {number}
  */
 var lastRemaining = function(n, m) {
+    let arr = [];
+    let pos = m-1;
+    let len = n;
+    for (let i=0; i<= n ; i++)
+    {
+        arr[i] = i;
+    }
+    while(len > 1)
+    {
+        if(pos >= len)
+        {
+            pos = pos - len;
+        }
+        else
+        {
+            arr.splice(pos,1);
+            len--;
+            pos = pos +m-1;
+        }
 
+
+    }
+    return arr[0];
 };
