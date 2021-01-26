8b16349e8b441339d7481f9eb99a6fe1c30ea565
TiaZHOU <tiazhou@student.unimelb.edu.au>
2021-01-26 18:06:48 +1100

    1191

--- 1191. K-Concatenation Maximum Sum.js
+++ 1191. K-Concatenation Maximum Sum.js
@@ -5,14 +5,16 @@
  * @return {number}
  */
 var kConcatenationMaxSum = function(arr, k) {
-
+    if(arr.every(x => x <= 0))
+    {
+        return 0;
+    }
     let sum = arr.reduce((a,b)=> a+b ,0);
     // all inter
     if(arr.every(x => x >= 0))
     {
         return sum*k% 1000000007;
     }
-
     if(k ==1)
     {
         return max_sub(arr);
@@ -28,6 +30,7 @@ var kConcatenationMaxSum = function(arr, k) {
             let max_pre = 0;
             let max_post =0;
             let temp=0;
+            let temp2=0;
             for(let i=0; i< arr.length; i++)
             {
                 temp = temp + arr[i];
@@ -35,17 +38,15 @@ var kConcatenationMaxSum = function(arr, k) {
                 {
                     max_pre =temp;
                 }
-            }
 
-            temp=0;
-            for(let i=arr.length-1; i>=0; i--)
-            {
-                temp = temp + arr[i];
-                if(temp > max_post)
+                temp2 = temp2 + arr[arr.length-1-i];
+                if(temp2 > max_post)
                 {
-                    max_post =temp;
+                    max_post =temp2;
                 }
             }
+
+
             let sub =  max_sub(arr.concat(arr));
             let sums = sum*(k-2);
             if(max_pre >= sum)
@@ -80,29 +81,51 @@ var kConcatenationMaxSum = function(arr, k) {
 var max_sub = function(arr){
     let pre  = [];
     pre[0]= arr[0];
+    let min = pre[0];
+    let max_sub =pre[0];
     for(let i=1; i< arr.length; i++)
     {
         pre[i] =  pre[i-1] + arr[i];
-    }
-    let max = pre[0];
-    let min = pre[0];
-    //largest pre_sub pos
-    let max_pos = 0;
-    //largest post_sub pos
-    let min_pos = 0;
-    let max_sub =pre[0];
-    for (let i=1; i< arr.length; i++)
-    {
+
         if(pre[i] - min > max_sub)
         {
             max_sub = pre[i]-min;
         }
         if(pre[i] < min)
         {
-            min_pos = i;
             min = pre[i];
         }
     }
     if(max_sub <0) return 0;
     return max_sub;
 };
+
+/**
+ * link:https://leetcode-cn.com/problems/k-concatenation-maximum-sum/
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var kConcatenationMaxSum2 = function (arr, k) {
+    let right = 0;
+    let left = 0;
+    let toLeftSum = 0;
+    let toRightSum = 0;
+    let max = 0;
+    for (let l = 0, len = arr.length - 1, curMax = 0, r = len; l <= len; ++l, --r) {
+        toRightSum += arr[l]
+        toLeftSum += arr[r]
+        curMax = Math.max(0, curMax + arr[l])
+        max = Math.max(max, curMax)
+        left = Math.max(left, toLeftSum)
+        right = Math.max(right, toRightSum)
+    }
+    if (k === 1) {
+        return max % 1000000007;
+    } else if (toRightSum <= 0) {
+        return Math.max(left + right, max) % 1000000007
+    } else {
+        return Math.max(toRightSum * (k - 2) + left + right, max) % 1000000007
+    }
+};
+
