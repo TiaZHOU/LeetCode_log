51348f58751b408954dcd3dffc156a32d3133b84
TiaZHOU <tiazhou@student.unimelb.edu.au>
2021-03-09 20:45:41 +1100

    1464

--- 1464. Maximum Product of Two Elements in an Array.js
+++ 1464. Maximum Product of Two Elements in an Array.js
@@ -4,23 +4,6 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let temp1 = 0;
-    let temp2 = 0;
-    for(let i=0; i<nums.length; i++)
-    {
-        if(nums[i] > temp2)
-        {
-            temp1 = temp2;
-            temp2 = nums[i];
-
-        }
-        else
-        {
-            if(nums[i] >= temp1)
-            {
-                temp1 = nums[i];
-            }
-        }
-    }
-    return (temp2-1)*(temp1 - 1);
+    nums.sort((a, b) => b - a);
+    return (nums[0]-1)*(nums[1]-1);
 };
