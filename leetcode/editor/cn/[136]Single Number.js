27cbf7fdda8a2c06be376c4c542a13bd392a8a48
terry <tiazhou@student.unimelb.edu.au>
2021-09-25 23:39:44 +1000

    136

---
+++ leetcode/editor/cn/[136]Single Number.js
@@ -0,0 +1,49 @@
+//Given a non-empty array of integers nums, every element appears twice except f
+//or one. Find that single one. 
+//
+// You must implement a solution with a linear runtime complexity and use only c
+//onstant extra space. 
+//
+// 
+// Example 1: 
+// Input: nums = [2,2,1]
+//Output: 1
+// Example 2: 
+// Input: nums = [4,1,2,1,2]
+//Output: 4
+// Example 3: 
+// Input: nums = [1]
+//Output: 1
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 3 * 104 
+// -3 * 104 <= nums[i] <= 3 * 104 
+// Each element in the array appears twice except for one element which appears 
+//only once. 
+// 
+// Related Topics 位运算 数组 
+// 👍 2037 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    if(nums.length === 1) return nums[0];
+    for(let i=0; i<nums.length;i++)
+    {
+        if(nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i]))
+        {
+            return nums[i];
+        }
+    }
+    return 0;
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
+console.log(singleNumber([4,1,2,1,2]));
