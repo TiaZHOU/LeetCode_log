34421442124fad49c70b707d617460f8c7bd85a1
terry <tiazhou@student.unimelb.edu.au>
2021-09-28 22:03:07 +1000

    169 update

--- leetcode/editor/cn/[169]Majority Element.js
+++ leetcode/editor/cn/[169]Majority Element.js
@@ -32,23 +32,21 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let  map = new Map();
-    for(let i=0; i< nums.length; i++)
+    let count  =  0;
+    let ans    =  0;
+    for(let i=0; i<nums.length; i++)
     {
-        if(map.has(nums[i]))
+        if(count === 0) 
         {
-            map.set(nums[i],map.get(nums[i])+1);
+            ans = nums[i];
+            count++;
         }
-        else
+        else 
         {
-            map.set(nums[i],1);
-        }
-        if(map.get(nums[i]) > nums.length/2)
-        {
-            return nums[i];
+            ans==nums[i]?count++:count--;
         }
     }
-    return 0;
+    return ans;
 };
 //leetcode submit region end(Prohibit modification and deletion)
 console.log(majorityElement([1,1,1,1,1,2,2,2,2,2,2,2,2,2]))
