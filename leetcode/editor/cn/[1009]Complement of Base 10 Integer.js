646ef673a8e7e2f0670a629485b6c842826834f6
TiaZHOU <xiaoxieabcd@hotmail.com>
2022-10-25 16:34:05 +1100

    1009

---
+++ leetcode/editor/cn/[1009]Complement of Base 10 Integer.js
@@ -0,0 +1,73 @@
+//The complement of an integer is the integer you get when you flip all the 0's 
+//to 1's and all the 1's to 0's in its binary representation. 
+//
+// 
+// For example, The integer 5 is "101" in binary and its complement is "010" 
+//which is the integer 2. 
+// 
+//
+// Given an integer n, return its complement. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: n = 5
+//Output: 2
+//Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2
+// in base-10.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: n = 7
+//Output: 0
+//Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0
+// in base-10.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: n = 10
+//Output: 5
+//Explanation: 10 is "1010" in binary, with complement "0101" in binary, which 
+//is 5 in base-10.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= n < 10⁹ 
+// 
+//
+// 
+// Note: This question is the same as 476: https://leetcode.com/problems/number-
+//complement/ 
+//
+// Related Topics 位运算 👍 83 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function(n) {
+    let temp =2;
+    while(true)
+    {
+        if(n >= temp)
+        {
+            temp = temp*2;
+        }
+        else
+        {
+            return temp - n -1;
+        }
+    }
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
